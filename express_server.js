7761970ee2d1fc3b67d6b03d16044b474855e826
Ebubekir CAKI <jaiudah@gmail.com>
2021-09-28 03:29:54 +0000

    urlpart1

--- express_server.js
+++ express_server.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+function generateRandomString() {
+
+}
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.listen(PORT, () => {
@@ -28,6 +37,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
