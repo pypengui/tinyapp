c2aa30d0ca3179c06b3d6385b3c91ef7dc66e1a6
Ebubekir CAKI <jaiudah@gmail.com>
2021-09-28 18:23:43 +0000

    w3d1 assignments

--- express_server.js
+++ express_server.js
@@ -6,12 +6,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 function generateRandomString() {
-
+  const rand = Math.random().toString(36).substr(2, 8);
+  return rand
 }
 
-
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -44,6 +44,9 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL
+  const newShortURL = generateRandomString()
+  urlDatabase[newShortURL] = longURL
+  console.log('urlDatabase', urlDatabase); // Log the POST request body to the console
+  res.redirect(`/urls/${newShortURL}`)
 });
